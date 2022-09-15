cad8c55859f8aaccc6c3eea7dc7a9e8eda46e422
dauqu <sujeetsingh734321@gmail.com>
2022-09-15 16:01:34 +0530

    Changes

--- main.js
+++ main.js
@@ -43,6 +43,14 @@ app.use(`${apiv1}/news`, require("./routes/news"));
 app.use(`${apiv1}/profile`, require("./routes/profile"));
 app.use(`${apiv1}/feed`, require("./routes/feed"));
 app.use(`${apiv1}/bookmarks`, require("./routes/bookmarks"));
+app.use(`${apiv1}/search`, require("./routes/search"));
+
+//Static files
+app.use(express.static("public"));
+
+app.use("/privacy-policy", (req, res) => {
+  res.sendFile(__dirname + "/public/privacy-policy.html");
+});
 
 
 app.listen(PORT, () => {
